e2a31abeacced5dfcabd0c2c24461daa67e01a1f
Satyabrat2005 <satyabratsahu71@gmail.com>
2025-07-24 14:30:42 +0530

    Update app.py

--- backend-ml/app.py
+++ backend-ml/app.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from model_utils import detect_emotion # type: ignore
+from model_utils import detect_emotion  # type: ignore
 from fastapi.middleware.cors import CORSMiddleware
-
 
 app = FastAPI()
 
@@ -13,17 +12,20 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-class emotionRequest(BaseModel):
+class EmotionRequest(BaseModel):
     text: str
 
+# API endpoint
 @app.post("/detect-emotion")
-async def detect_emotion_api(req: emotionRequest):
+async def detect_emotion_api(req: EmotionRequest):
     emotion = detect_emotion(req.text)
+
     response = {
         "anxious": "You sound anxious. It’s okay, you’re safe and not alone.",
         "frustrated": "You seem frustrated. Take your time, I’m here to help.",
         "calm": "That’s good to hear. Let me know if you need anything.",
         "exhausted": "You might need rest. You’re doing okay.",
+        "disoriented": "It looks like you're unsure where you are. Let me remind you, you're at home and you're safe.",
         "neutral": "I’m with you. Everything is okay."
     }.get(emotion, "I'm here with you.")
 
